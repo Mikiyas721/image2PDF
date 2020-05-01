7b1cbbcc6fe424b817308c589328049c7596587e
Mikiyas721 <Mikiyas721@gmail.com>
2020-05-01 14:08:58 -0600

    Added project

---
+++ src/bot.js
@@ -0,0 +1,16 @@
+const Telegraf = require('telegraf');
+const axios = require('axios');
+
+const bot = new Telegraf('1111631380:AAFDD02uWSR6pXLwb8xoyGGpG5FeHYqhpyI');
+
+const helpString = `*Image to PDF Bot*
+This bot helps you convert your image files to PDF with only a few steps.`;
+
+bot.command('start', ctx => {
+    ctx.telegram.sendMessage(ctx.chat.id, helpString, {
+        parse_mode: "MarkdownV2"
+    })
+});
+
+
+bot.launch();
