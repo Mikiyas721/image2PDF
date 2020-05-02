6565a21264790496da33a7ea56a160b1f8b53146
Mikiyas721 <Mikiyas721@gmail.com>
2020-05-02 11:48:19 -0600

    Added minimum functionality

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const Telegraf = require('telegraf');
 const axios = require('axios');
+const api = require('convertapi')('XnxiVRXLqFjv9yRm');
 
 const bot = new Telegraf('1111631380:AAFDD02uWSR6pXLwb8xoyGGpG5FeHYqhpyI');
 
@@ -10,6 +11,12 @@ bot.command('start', ctx => {
     ctx.telegram.sendMessage(ctx.chat.id, helpString, {
         parse_mode: "MarkdownV2"
     })
+});
+
+bot.on('photo', async ctx => {
+    const userImageLink = await ctx.telegram.getFileLink(ctx.update.message.photo[(ctx.update.message.photo.length) - 1].file_id);
+    const resultPromise = await api.convert('pdf', {File: userImageLink});
+    ctx.replyWithDocument(resultPromise.response.Files[0].Url)
 });
 
 
